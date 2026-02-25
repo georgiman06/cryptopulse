65894f5e6a34c3c3b3100544f4ba77f47ea5f136
Georgi Manianchira <georgiman.1234@gmail.com>
2026-02-25 16:02:22 -0500

    fix duplicate coin_id by keeping latest record

--- models/silver/silver_crypto_prices.sql
+++ models/silver/silver_crypto_prices.sql
@@ -5,8 +5,16 @@ WITH raw AS (
         coin_id,
         coin_name,
         ingested_at,
-        raw_data
+        raw_data,
+        ROW_NUMBER() OVER (
+            PARTITION BY coin_id 
+            ORDER BY ingested_at DESC
+        ) AS rn
     FROM cryptopulse_catalog.bronze.crypto_raw
+),
+
+latest AS (
+    SELECT * FROM raw WHERE rn = 1
 ),
 
 parsed AS (
@@ -21,7 +29,7 @@ parsed AS (
         CAST(get_json_object(raw_data, '$.price_change_24h') AS DOUBLE)            AS price_change_24h,
         CAST(get_json_object(raw_data, '$.price_change_percentage_24h') AS DOUBLE) AS price_change_percentage_24h,
         get_json_object(raw_data, '$.last_updated')                  AS last_updated
-    FROM raw
+    FROM latest
 )
 
 SELECT * FROM parsed
